5b5917612174149b53ad0af7d6a9b9612e682ac0
Marek Freunscht <Freunscht58@outlook.de>
2022-06-20 19:14:13 +0200

    Initial empty commit

---
+++ src/Window.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Cxmu03 on 20/06/2022.
+//
+
+#include "Window.h"
---
+++ src/Window.h
@@ -0,0 +1,14 @@
+#ifndef OWL_WINDOW_H
+#define OWL_WINDOW_H
+
+#include <memory>
+
+namespace owl {
+
+class Window {
+
+};
+
+}
+
+#endif //OWL_WINDOW_H
