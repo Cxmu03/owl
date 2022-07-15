17e635373e8a9552fae682c9c576671ed6199e14
Marek Freunscht <Freunscht58@outlook.de>
2022-07-15 02:37:34 +0200

    Extended Utils

--- owl/Util.hpp
+++ owl/Util.hpp
@@ -1,13 +1,26 @@
 #ifndef OWL_UTIL_HPP
 #define OWL_UTIL_HPP
 
-namespace owl {
+#include <string>
+#include <string_view>
+#include <fstream>
+#include <sstream>
+
+namespace owl::util {
 
 template<typename T>
 static constexpr inline T
 MapToRange(T value, T valueRangeStart, T valueRangeEnd, T resultRangeStart, T resultRangeEnd) {
     return resultRangeStart +
            (((value - valueRangeStart) / (valueRangeEnd - valueRangeStart)) * (resultRangeEnd - resultRangeStart));
+}
+
+static inline std::string ReadFromFile(std::string_view path) {
+    std::ifstream fileStream(path.data());
+    std::stringstream stringStream;
+    stringStream << fileStream.rdbuf();
+    std::string fileContents = stringStream.str();
+    return fileContents;
 }
 
 }
