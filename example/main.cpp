88028dd4e059b1aa1d0dc67e6496398eb95675a7
Marek Freunscht <Freunscht58@outlook.de>
2022-07-17 03:20:27 +0200

    Added common owl header file

--- example/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "../owl/Window.hpp"
-
-int main() {
-    owl::Window window({500, 500}, "Poggers");
-    window.Run([](){});
-}
---
+++ owl/owl.hpp
@@ -0,0 +1,9 @@
+#ifndef OWL_OWL_HPP
+#define OWL_OWL_HPP
+
+#include "Window.hpp"
+#include "Shader.hpp"
+#include "Vec2.hpp"
+#include "Color.hpp"
+
+#endif //OWL_OWL_HPP
